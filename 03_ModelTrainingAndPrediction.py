c18b1c7e14b220a62c6a0f7150aa6c2509166c57
Dawid Borycki <dawid@borycki.com.pl>
2021-10-24 18:40:56 +0200

    linear regressionn

---
+++ 03_ModelTrainingAndPrediction.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Read train data
+dataFrame = pd.read_csv('train_data.csv',sep=';')
+
+# Use DepartureHour column as X
+X = dataFrame.DepartureHour
+
+# Use TravelTimeInHours column as Y
+Y = dataFrame.TravelTimeInHours
+
+# Train the model
+from sklearn.linear_model import LinearRegression
+model = LinearRegression().fit(np.array(X).reshape(-1,1), Y)
+
+# Predict travel time for different departure hours
+departureHours = [1.5, 3.12, 5.17, 16.05, 10.15, 20.29]
+
+for departureHour in departureHours:
+    predictedTravelTime = model.predict(np.array(departureHour).reshape(-1,1))
+
+    print(f"Departure hour: {departureHour},\t Predicted travel time: {predictedTravelTime[0]:.2f} h")

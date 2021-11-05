1aa33d269142023dc4bb0dea506440baf5b36542
Dawid Borycki <dawid@borycki.com.pl>
2021-11-05 12:10:40 +0100

    vehicleCommercial

--- 01_GetData.py
+++ 01_GetData.py
@@ -9,6 +9,7 @@ traffic = "true"                             # To include Traffic information
 travelMode = "truck"                         # Travel by truck
 avoid = "unpavedRoads"                       # Avoid unpaved roads
 departAt = "2021-10-20T10:00:00"             # Departure date and time
+vehicleCommercial = "true"                   # Commercial vehicle 
 key = "<TYPE_YOUR_API_KEY_HERE>"             # API Key
 
 # Building the request URL
@@ -20,6 +21,7 @@ requestParams = (
     + "&traffic=" + traffic
     + "&travelMode=" + travelMode
     + "&avoid=" + avoid 
+    + "&vehicleCommercial=" + vehicleCommercial
     + "&departAt=" + urlparse.quote(departAt))
 
 requestUrl = baseUrl + requestParams + "&key=" + key
